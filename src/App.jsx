8860cd7db594f99c7a09f149918e9032fc4ef8c8
Karl Frick <karl.frick5@gmail.com>
2021-06-03 19:10:30 -0400

    Each numbered button is hardcoded with their numerical value

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react'
 
 export function App() {
   // define STATE and setSTATE of the value of the computed total
-  const [total, setTotal] = useState(100)
+  const [total, setTotal] = useState(55)
+  const [number, setNumber] = useState({ total })
 
   function handleAC() {
     setTotal(0)
+  }
+
+  function handleNumClick(event) {
+    console.log(event.target.value)
   }
 
   return (
@@ -19,19 +24,39 @@ export function App() {
           <button className="button fn">&plusmn;</button>
           <button className="button fn">%</button>
           <button className="button op">&divide;</button>
-          <button className="button">7</button>
-          <button className="button">8</button>
-          <button className="button">9</button>
+          <button className="button" value={7} onClick={handleNumClick}>
+            7
+          </button>
+          <button className="button" value={8} onClick={handleNumClick}>
+            8
+          </button>
+          <button className="button" value={9} onClick={handleNumClick}>
+            9
+          </button>
           <button className="button op">&times;</button>
-          <button className="button">4</button>
-          <button className="button">5</button>
-          <button className="button">6</button>
+          <button className="button" value={4} onClick={handleNumClick}>
+            4
+          </button>
+          <button className="button" value={5} onClick={handleNumClick}>
+            5
+          </button>
+          <button className="button" value={6} onClick={handleNumClick}>
+            6
+          </button>
           <button className="button op">&minus;</button>
-          <button className="button">1</button>
-          <button className="button">2</button>
-          <button className="button">3</button>
+          <button className="button" value={1} onClick={handleNumClick}>
+            1
+          </button>
+          <button className="button" value={2} onClick={handleNumClick}>
+            2
+          </button>
+          <button className="button" value={3} onClick={handleNumClick}>
+            3
+          </button>
           <button className="button op">+</button>
-          <button className="button x2">1</button>
+          <button className="button x2" value={0} onClick={handleNumClick}>
+            0
+          </button>
           <button className="button">.</button>
           <button className="button op">=</button>
         </div>
